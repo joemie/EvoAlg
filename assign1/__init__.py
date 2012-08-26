4598d164f8ae7a870a41dca0a32127464a6ce9e0
Joseph <joemie92@gmail.com>
2012-08-26 13:19:54 -0500

    Config File Implemented

--- assign1/__init__.py
+++ assign1/__init__.py
@@ -1,53 +1,60 @@
 import logging
 import time
+import sys
 import random
 from graph import *
 from decimal import Decimal
 
-logging.basicConfig(level = logging.DEBUG, filename= 'log.txt')
-
 startTime = Decimal(time.time() * 1000)
-mySeed = 1
-myCut = []
-random.seed(mySeed)
+cut = []
+args = sys.argv[1:]
 
-myFile = open(r'C:\Users\joseph\MiscDev\Python\EvoAlg\assign1\dataFiles\example.dat')
-myFileBuffer = myFile.read().splitlines()
-logging.warning("---RUN START : %s" % startTime)
-logging.warning("---RUN SEED  : %s" % mySeed)
-logging.warning("---INPUT FILE: %s" % myFile)
-#logging.debug('in de boog')
-
-numNodes = myFileBuffer[0]
-numEdges = myFileBuffer[1]
-numCuts = 0
-myEdges = buildGraph(myFileBuffer[2:])
-subNodes1 = []
-subNodes2 = []
-for i in range(int(numNodes)):
-    subGroup = random.randrange(2)
-    myCut.append(subGroup)
-    if subGroup == 0:
-        subNodes1.append(i + 1)
+if len(args) == 0:
+    print "NO CONFIG FILE SPECIFIED"
+else:
+    configFile = open(str(args[0]))
+    configBuffer = configFile.read().splitlines()
+    inFile = open(configBuffer[0])
+    inFileBuffer = inFile.read().splitlines()
+    if int(configBuffer[1]) == 00:
+        seed = startTime
     else:
-        subNodes2.append(i + 1)
-print myEdges
-print myCut
-print subNodes1
-print subNodes2
+        seed = configBuffer[1]
+    random.seed(seed)
+    numRuns = configBuffer[2]
+    logging.basicConfig(level=logging.DEBUG, filename=configBuffer[3])
+    solutionFile = open(configBuffer[4], 'w')
+    logging.warning("---RUN START : %s" % startTime)
+    logging.warning("---RUN SEED  : %s" % seed)
+    logging.warning("---INPUT FILE: %s" % inFile)
+    #logging.debug('in de boog')
 
-for edgeIter in range(len(myEdges)):
-    tStart = int(myEdges[edgeIter][0])
-    tEnd = int(myEdges[edgeIter][1])
-    if tStart in subNodes1 and tEnd not in subNodes1:
-        numCuts += 1
-    elif tStart in subNodes2 and tEnd not in subNodes2:
-        numCuts += 1
-print float(numCuts) / min(myCut.count(0), myCut.count(1))
-myFile.close
+    numNodes = inFileBuffer[0]
+    numEdges = inFileBuffer[1]
+    numCuts = 0
+    edges = buildGraph(inFileBuffer[2:])
+    subNodes1 = []
+    subNodes2 = []
 
-endTime = Decimal(time.time() * 1000)
-logging.warning("---RUN END   : %s" % endTime)
-logging.warning("---RUN Length: %s" % (endTime - startTime))
-logging.warning("---RUN SEED  : %s" % mySeed)
-print "DONE"
+    for i in range(int(numNodes)):
+        subGroup = random.randrange(2)
+        cut.append(subGroup)
+        if subGroup == 0:
+            subNodes1.append(i + 1)
+        else:
+            subNodes2.append(i + 1)
+
+    print edges
+    print cut
+    print subNodes1
+    print subNodes2
+
+    fitness = calculateFitness(edges, subNodes1, subNodes2, cut)
+    print fitness
+    inFile.close
+
+    endTime = Decimal(time.time() * 1000)
+    logging.warning("---RUN END   : %s" % endTime)
+    logging.warning("---RUN Length: %s" % (endTime - startTime))
+    logging.warning("---RUN SEED  : %s" % seed)
+    print "DONE"
--- assign1/graph.py
+++ assign1/graph.py
@@ -6,3 +6,14 @@ def buildGraph(myFileBuffer):
         endNode = line[line.index(" "):]
         edges.append([startNode, endNode])
     return edges
+
+def calculateFitness(edges, subNodes1, subNodes2, cut):
+    numCuts = 0
+    for edgeIter in range(len(edges)):
+        tStart = int(edges[edgeIter][0])
+        tEnd = int(edges[edgeIter][1])
+        if tStart in subNodes1 and tEnd not in subNodes1:
+            numCuts += 1
+        elif tStart in subNodes2 and tEnd not in subNodes2:
+            numCuts += 1
+    return float(numCuts) / min(cut.count(0), cut.count(1))

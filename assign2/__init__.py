ec4a8e480e01580dd05c7f11759865faaca5785a
Joseph <joemie92@gmail.com>
2012-09-09 22:15:56 -0500

    Fixed Seed Issue

--- assign2/__init__.py
+++ assign2/__init__.py
@@ -9,6 +9,7 @@ from graph import *
 from arena import *
 from decimal import Decimal
 
+
 startTime = Decimal(time.time() * 1000)
 args = sys.argv[1:]
 configFile = None
@@ -30,29 +31,46 @@ else:
 random.seed(seed)
 numRuns = configBuffer[2]
 numEvals = configBuffer[3]
+logFile = open(str(configBuffer[4]), 'a+')
 solutionFile = open(str(configBuffer[5]), 'a+')
-parentSelType = str(configBuffer[6])
-recombType = str(configBuffer[7])
-survivalType = str(configBuffer[8])
+pSelType = str(configBuffer[6]).partition("|")[0].strip()
+if(pSelType == "tournament"):
+    pTournSize = str(configBuffer[6].partition("|")[2])
+else:
+    pTournSize = None
+recombType = str(configBuffer[7]).partition("|")[0].strip()
+if(recombType == "npoint"):
+    numSplits = str(configBuffer[7].partition("|")[2])
+else:
+    numSplits = None
+survivalType = str(configBuffer[8]).partition("|")[0].strip()
+if(survivalType == "tournament"):
+    survivalTournSize = str(configBuffer[8].partition("|")[2])
+else:
+    survivalTournSize = None
+parentSize = int(configBuffer[9])
+childrenSize = int(configBuffer[10])
 
 print configFile
 print logFile
 logFile.write("SESSION START : %s" % startTime + "\n")
 logFile.write("SESSION SEED  : %s" % seed + "\n")
 logFile.write("CONFIG FILE : %s" % inFile + "\n")
-logFile.write("PARENT SELECTION : %s" % parentSelType + "\n")
+logFile.write("SURVIVAL SELECTION: %s" % survivalType + "\n")
+logFile.write("PARENT SELECTION : %s" % pSelType + "\n")
 logFile.write("RECOMBINATION: %s" % recombType + "\n")
 logFile.write("MUTATION: BIT FLIP")
-logFile.write("SURVIVAL SELECTION: %s" % survivalType + "\n")
+logFile.write("POPULATION SIZE: " + str(parentSize) + "\n")
+logFile.write("OFFSPRING SIZE: " + str(childrenSize) + "\n")
 numNodes = inFileBuffer[0]
 numEdges = inFileBuffer[1]
 numCuts = 0
 edges = buildGraph(inFileBuffer[2:])
 bestCut = []
-population = initPopulation(5, 8)
-
+population = initPopulation(500, 8)
+selectParents(edges, population, childrenSize, pSelType, survivalTournSize)
 for i in range(int(numRuns)):
-    bestFitness = 100
+    bestFitness = float("-inf")
     logFile.write("RUN: " + str(i + 1) + "\n")
     print("RUN: " + str(i + 1))
     for j in range(int(numEvals) + 1):
@@ -62,13 +80,12 @@ for i in range(int(numRuns)):
         #tTime = Decimal(time.time() * 1000)
         fitness = calculateFitness(edges, cut)
         #print "CALC FIT: " + str(Decimal(time.time() * 1000) - tTime)
-        if fitness < bestFitness:
+        if fitness > bestFitness:
             bestFitness = fitness
             bestCut = cut
             logFile.write('\t' + str(j + 1) + '\t' + str(fitness) + '\n')
-solutionFile.write(str(bestFitness) + "\n")
+solutionFile.write(str(bestFitness * -1) + "\n")
 solutionFile.write(str(bestCut) + "\n")
-
 solutionFile.close()
 inFile.close
 endTime = Decimal(time.time() * 1000)
--- assign2/arena.py
+++ assign2/arena.py
@@ -1,11 +1,28 @@
 import random
-
+from operator import itemgetter
+import sys
 
 def initPopulation(popSize, strSize):
     pop = []
     for i in range(popSize):
         pop.append(list(bin(random.randrange(2 ** (int(strSize) - 1), 2 ** int(strSize)))[2:]))
     return pop
+
+def selectParents(edges, population, childrenSize, selType, tournSize = None):
+    selected = []  #list of indexes in the popualtion
+    if selType == "tournament" and tournSize != None:
+        for i in range(int(childrenSize)):
+            tournSelect = []
+            for j in range(int(tournSize)):
+                popIndex = random.randint(0, len(population) - 1)
+                appendMe = {"index" : popIndex , "fitness": calculateFitness(edges, population[popIndex]) * -1}
+                tournSelect.append(appendMe)
+            tournSelect = sorted(tournSelect, key=itemgetter('fitness'))
+            print tournSelect
+    elif selType == "tournament" and tournSize == None:
+        print "INVALID TOURNAMENT SIZE - EXITING"
+        sys.exit()
+    return selected
 
 def calculateFitness(edges, cut):
     numCuts = 0
@@ -15,7 +32,6 @@ def calculateFitness(edges, cut):
             if cut[int(key) - 1] != cut[int(value) - 1]:
                 numCuts += 1
     if numCuts == 0:
-        return float("inf")  # the graph wasn't cut so return infinity
+        return float("-inf")  # the graph wasn't cut so return infinity
     else:
-        return float(numCuts / 2) / min(cut.count('0'), cut.count('1'))
-
+        return float(numCuts / 2) / min(cut.count('0'), cut.count('1')) * -1
